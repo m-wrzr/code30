31fc4743afaf0530c895e01a04920cf08d327d78
Martin Wurzer <martin.wrzr@gmail.com>
2016-04-13 13:48:03 +0200

    setup for 1; adjusted table

---
+++ 01/solution.py
@@ -0,0 +1,10 @@
+# https://code.google.com/codejam/contest/635101/dashboard#s=p0
+
+
+# input() reads a string with a line of input, stripping the '\n' (newline) at the end.
+# This is all you need for most Google Code Jam problems.
+t = int(input())  # read a line with a single integer
+for i in range(1, t + 1):
+    n, m = [int(s) for s in input().split(" ")]  # read a list of integers, 2 in this case
+    print("Case #{}: {} {}".format(i, n + m, n * m))
+    # check out .format's specification for more formatting options
